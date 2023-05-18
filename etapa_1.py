b7e4440798d02e4c243ac814e265058c999fa9ca
Alex Limachi <alimachi@fi.uba.ar>
2023-05-17 21:05:06 -0300

    etapa 1

--- etapa_1.py
+++ etapa_1.py
@@ -1,8 +1,9 @@
 # ETAPA_1
 # INTERACCION CON EL JUGADOR
 
-def tablero(lista_letras_a_jugar,lista_acierto,letra_errada,letra_adivinada):
-    for element in lista_letras_a_jugar:
+def tablero(letra,lista_acierto,letra_errada,letra_adivinada):
+
+    for element in letra:
         print(f"["+element+"]",end="")
     print() 
     for acetado in lista_acierto:
@@ -10,7 +11,23 @@ def tablero(lista_letras_a_jugar,lista_acierto,letra_errada,letra_adivinada):
     print()
     print(f"Acierto: {letra_adivinada}")
     print(f"Errores: {letra_errada}")
-tablero(["a","b"],[[]],0,1)
+
+def turno(letra,long_palabra,deficion):
+    LETRA_CAND = 0
+    print(f"Turno de letra {letra[LETRA_CAND]} - Palabra de {long_palabra} letras")
+    print(f"Definicion: {deficion}")
+
+
+def ingresar_palabra(palabra_cand):
+    palabra = input("Ingrese palabra:")
+    lista_vocal_acent = ["á", "é", "í", "ó", "ú"]
+
+            
+
+
+
+
+
 
 
 
