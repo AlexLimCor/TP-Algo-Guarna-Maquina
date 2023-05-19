0fe119b0aafa1933b8e0987f54c005bac6820615
Alex Limachi <alimachi@fi.uba.ar>
2023-05-19 19:59:09 -0300

    Etapa 2

--- etapa_1.py
+++ etapa_1.py
@@ -1,3 +1,5 @@
+from datos import obtener_lista_definiciones
+
 # ETAPA_1
 # INTERACCION CON EL JUGADOR
 
@@ -16,7 +18,7 @@ def tablero(letras_candidatas):
     print(f"Errores: {ERRORES}")
 
 def turno(palabra_y_deficion):
-    # OBTENEMOS EL DICCIONARIO CON SU PALABRA Y DEFINICION (ETAPA 2) 
+    # OBTENEMOS EL DICCIONARIO CON SU PALABRA Y DEFINICION 
     LETRA_CAND = 0
     DEFINICION = 1
     print(f"Turno de letra {palabra_y_deficion[LETRA_CAND]} - Palabra de {len(palabra_y_deficion[LETRA_CAND])} letras")
@@ -25,6 +27,28 @@ def turno(palabra_y_deficion):
 def ingre_palabra(palabras_candidatas):
     # ETAPA 5 , PUNTAJE 
     palabra = input("Ingrese palabra:")
+
+# ETAPA 2
+'''En esta etapa procesamos el diccionario de palabras con sus definiciones y lo convertimos a diccionario con los requisitos pedidos'''
+def etapa_2():
+    datos = obtener_lista_definiciones()
+    dicc_pal_def = {}
+    LONG_MIN = 5
+    PALABRA = 0
+    DEFINICION = 1
+    for element in datos:
+        clave = element[PALABRA]
+        valor = element[DEFINICION]
+        if len(clave) >= LONG_MIN:
+            dicc_pal_def[clave] = valor
+    return dicc_pal_def
+print(etapa_2())
+
+
+
+
+
+
 
         
 
