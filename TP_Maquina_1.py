6fd9ad4ebf26adeeafb444f6c07c33b0c419f022
Alex Limachi <alimachi@fi.uba.ar>
2023-05-21 18:07:32 -0300

    Uno todas las etapas

--- TP_Maquina_1.py
+++ TP_Maquina_1.py
@@ -1,8 +1,8 @@
-from datos import obtener_lista_definiciones
-import random
+from etapa3 import definicion, etapa_2, etapa3 , letrasElegidas
+# UNIMOS TODAS LAS ETAPAS CREADAS 
 
 def invocar():
-    deficion_lista = obtener_lista_definiciones()
+    palabras_deficiones = definicion(etapa3(etapa_2(),letrasElegidas))
     lista_aciertos = []
     ACIERTOS = 0
     lista_informe = []
@@ -11,54 +11,51 @@ def invocar():
     CARACTER = 0
     ACERTADO = "[a]"
     ERRADO = "[e]"
-    CARACTER_A_JUGAR = 0
+    PALABRA = 0
     INDICE =0
-    DEFINICION_JUGAR = 1
-    DEFINICION = 0
-    lista_jugar = random.sample(deficion_lista,10)
-    while INDICE < len(lista_jugar):
-        for caracter in lista_jugar:
-            print("["+caracter[CARACTER_A_JUGAR][CARACTER_A_JUGAR].upper()+"]",end="")   
+    PALABRA_UNO = 0
+    DEFINICION = 1
+    while INDICE < len(palabras_deficiones):
+        for caracter in palabras_deficiones:
+            print("["+caracter[PALABRA_UNO][CARACTER].upper()+"]",end="")   
         print()
         for agregar in lista_aciertos:
             print(agregar,end="")
         print(f"\nAciertos:", ACIERTOS)
         print(f"Errores:",ERRORES)
-        print(f"Turno letra",lista_jugar[CARACTER][CARACTER_A_JUGAR][CARACTER_A_JUGAR].upper(),"- Palabra de",len(lista_jugar[CARACTER][CARACTER_A_JUGAR]),"letras")
-        print(f"Definicion:",lista_jugar[DEFINICION][DEFINICION_JUGAR])
+        print(f"Turno letra",palabras_deficiones[PALABRA][PALABRA_UNO][CARACTER].upper(),"- Palabra de",len(palabras_deficiones[PALABRA][PALABRA_UNO]),"letras")
+        print(f"Definicion:",palabras_deficiones[PALABRA][DEFINICION])
         ing_palabra = input(f"Ingrese palabra:")
-        ing_palabra = ing_palabra.lower()
-        if ing_palabra ==  lista_jugar[CARACTER][CARACTER_A_JUGAR]:
-            lista_informe.append(lista_jugar[CARACTER][CARACTER_A_JUGAR])
+        if ing_palabra ==  palabras_deficiones[PALABRA][PALABRA_UNO]:
+            lista_informe.append(palabras_deficiones[PALABRA][PALABRA_UNO])
             ACIERTOS +=1
-            CARACTER +=1
-            DEFINICION += 1
+            PALABRA +=1
             lista_aciertos.append(ACERTADO) 
         else:
-            lista_informe.append(lista_jugar[CARACTER][CARACTER_A_JUGAR])
+            lista_informe.append(palabras_deficiones[PALABRA][PALABRA_UNO])
             lista_agre_usuario.append(ing_palabra)
             ERRORES +=1
-            CARACTER +=1
-            DEFINICION +=1
+            PALABRA+=1
             lista_aciertos.append(ERRADO)
         print("----------------------------------------------------------")
         INDICE += 1
+    # ETAPA 5 , PUNTAJE Y RESUMEN
     print("*****RESUMEN *****")
     diccionario_resumen = dict(zip(lista_informe,lista_aciertos))
     INDICE_2 = 0
     PUNTAJE = 0
     for clave,valor in diccionario_resumen.items():
         if valor == ACERTADO:
-            print("Turno letra:",clave[CARACTER_A_JUGAR][CARACTER_A_JUGAR].upper(),"- Palabra de",len(clave),"letras-",clave,"acierto")
+            print("Turno letra:",clave[PALABRA_UNO][CARACTER].upper(),"- Palabra de",len(clave),"letras-",clave,"acierto")
             PUNTAJE +=10
         if valor == ERRADO:
-            print("Turno letra:",clave[CARACTER_A_JUGAR][CARACTER_A_JUGAR].upper(),"- Palabra de",len(clave),"letras-",lista_agre_usuario[INDICE_2],"- error - Palabra correcta:",clave)
+            print("Turno letra:",clave[PALABRA_UNO][CARACTER].upper(),"- Palabra de",len(clave),"letras-",lista_agre_usuario[INDICE_2],"- error - Palabra correcta:",clave)
             INDICE_2 +=1
             PUNTAJE -= 3
     print(f"Puntaje final: {PUNTAJE}")
 invocar()
 
-definiciones = obtener_lista_definiciones()
+'''definiciones = obtener_lista_definiciones()
 
 palabras = list()
 lista_a = list()
@@ -157,7 +154,7 @@ contador = 0
 for clave, valor in dicc_de_palabras.items():
     print("La letra",clave,"tiene",len(valor),"palabras")
     contador += len(valor)
-print("La cantidad de palabras en total que hay en el diccionario son:",contador)
+print("La cantidad de palabras en total que hay en el diccionario son:",contador)'''
 
 
    
