12a637d52e04064d5f4b265048c48df1710bf74f
Ariel <aclcruz5@gmail.com>
2023-07-08 00:21:18 -0300

    coreccion de rutas y doctest

--- etapa8.py
+++ etapa8.py
@@ -1,5 +1,7 @@
+from etapa3 import seleccionar_palabras_aleatoria
 from etapa10 import designar_configuracion
-from etapa3 import integrar_etapa_3
+import doctest
+import os
 
 #_________________________Funciones complementarias___________________________
 def orden_alfabetico(elemento):
@@ -54,17 +56,23 @@ def orden_alfabetico(elemento):
 def extraer_claves_coincidentes(diccionario,lista_palabras):
     """
     La funcion recibe como parametro un diccionario y una lista de palabras 
-    y devuelve una lista de listas con la palabra y su definicion
+    y devuelve una lista de listas con la palabra y su definicion en orden alfabetico
+    
+    >>> extraer_claves_coincidentes({"hola":"saludo","casa":"lugar donde se habita"},["hola","casa"])
+    [['casa', 'lugar donde se habita'], ['hola', 'saludo']]
+    >>> extraer_claves_coincidentes({"alcohol":"sustancia","casa":"lugar donde se habita"},["alcohol","casa"])
+    [['alcohol', 'sustancia'], ['casa', 'lugar donde se habita']]
+    >>> extraer_claves_coincidentes({"ceremonia":"ritual","raton":"dispositivo de entrada"},["ceremonia","raton"])
+    [['ceremonia', 'ritual'], ['raton', 'dispositivo de entrada']]
     """
-    lista_definiciones = []
+    lista_clave_valor = []
     for palabra, definicion in diccionario.items():
         if palabra in lista_palabras:
-            lista_definiciones.append([palabra,definicion])
-        lista_definiciones = sorted(lista_definiciones, key=lambda x: orden_alfabetico(x[0]))
-    return lista_definiciones
+            lista_clave_valor.append([palabra,definicion])
+        lista_clave_valor = sorted(lista_clave_valor, key=lambda x: orden_alfabetico(x[0]))
+    return lista_clave_valor
     #CRUZ, ARIEL CARLOS LEONARDO​
 
-excepciones = ["á","é","í","ó","ú","ñ"]
 
 def leer_archivo(archivo, default):
     """
@@ -75,9 +83,16 @@ def leer_archivo(archivo, default):
     return linea if linea else default
     #CRUZ, ARIEL CARLOS LEONARDO​
 
-def generador_diccionario(datos):
+def transformador_lista_a_dicc(datos):
     """
     Genera un diccionario con las palabras que cumplen con la condicion
+
+    >>> transformador_lista_a_dicc([["hola","saludo"],["casa","lugar donde se habita"]])
+    {'hola': 'saludo', 'casa': 'lugar donde se habita'}
+    >>> transformador_lista_a_dicc([["manzana","fruta"],["pera","fruta"],["casa","lugar donde se habita"]])
+    {'manzana': 'fruta', 'pera': 'fruta', 'casa': 'lugar donde se habita'}
+    >>> transformador_lista_a_dicc([["arbol","planta"],["casa","lugar donde se habita"]])
+    {'arbol': 'planta', 'casa': 'lugar donde se habita'}
     """
     palabras_candidatas = {}
     PALABRA = 0
@@ -98,14 +113,21 @@ def escribir_archivo(archivo, lista):
         archivo.write(f"{palabra},'{definicion}'\n")
     #CRUZ, ARIEL CARLOS LEONARDO​
 
-def escribir_diccionario(longitud_minima_palabra):
+#_________________________Rutas de archivos____________________________________
+definiciones_arc = os.path.join("definiciones_palabras","definiciones.txt")
+palabras_arc = os.path.join("definiciones_palabras","palabras.txt")
+diccionario_arc = os.path.join("definiciones_palabras","diccionario.csv")
+
+def escribir_dicc_csv(longitud_minima_palabra):
+    
+    
     """
     Parametros: numero entero
     La funcion recibe como parametro un numero entero y escribe en un archivo csv las palabras y sus definiciones
     """
-    palabras = open("definiciones_palabras\\palabras.txt","r",encoding='utf-8')
-    definiciones = open("definiciones_palabras\\definiciones.txt","r",encoding='utf-8')
-    diccionario = open("definiciones_palabras\\definiciones.csv","w",encoding='utf-8')
+    palabras = open(palabras_arc,"r",encoding='utf-8')
+    definiciones = open(definiciones_arc,"r",encoding='utf-8')
+    diccionario = open(diccionario_arc,"w",encoding='utf-8')
     palabras_candidatas = []
     linea_palabra = leer_archivo(palabras,"####")
     linea_definicion = leer_archivo(definiciones,"####")
@@ -140,7 +162,7 @@ def leer_diccionario():
     La funcion lee el archivo csv y devuelve una lista de listas con las palabras y sus definiciones
     """
     lista_palabras = []
-    with open("definiciones_palabras\\definiciones.csv","r",encoding='utf-8') as diccionario:
+    with open(diccionario_arc,"r",encoding='utf-8') as diccionario:
         linea = leer_archivo(diccionario,"####")
         linea_dicc = linea.rstrip("\n").split(",")
         while linea != "####":
@@ -150,15 +172,47 @@ def leer_diccionario():
     return lista_palabras
     #CRUZ, ARIEL CARLOS LEONARDO​
 
-def integrar_etapa_8():
-    diccionario_conf = designar_configuracion()
-    LONGITUD_PALABRA_MINIMA = int(diccionario_conf["LONGITUD_PALABRA_MINIMA"])
-    CANTIDAD_LETRAS_ROSCO = int(diccionario_conf["CANTIDAD_LETRAS_ROSCO"])
-    escribir_diccionario(LONGITUD_PALABRA_MINIMA)
-    lista_palabras = leer_diccionario()
-    dicc = generador_diccionario(lista_palabras)
 
-    letras,palabras = integrar_etapa_3(list(dicc.keys()),CANTIDAD_LETRAS_ROSCO)
-    palabras_definicion = extraer_claves_coincidentes(dicc,palabras)
-    return letras,palabras_definicion
+
+def crear_dicc_palabras_candidatas(longitud_minima_palabra):
+    """
+    La funcion escribe un archivo csv con las palabras y sus definiciones y devuelve un diccionario con las palabras y sus definiciones
+    Parametros: numero entero
+    return: diccionario con clave: palabra, valor: definicion
+    """
+    escribir_dicc_csv(longitud_minima_palabra)
+    lista_palabras_def_candidatas = leer_diccionario()
+    dicc_palabras_def_candidatas = transformador_lista_a_dicc(lista_palabras_def_candidatas)
+    return dicc_palabras_def_candidatas
     #CRUZ, ARIEL CARLOS LEONARDO​
+
+def generar_dicc_palabras_def_elegidas(diccionario_palabras_definiciones,cantidad_letras_rosco):
+    """
+    La elige las palabras que van a participar en el juego
+    Parametros: diccionario {palabra:definicion, ...}
+                numero entero. Cantidad de letras que va a tener el rosco
+    return: diccionario con clave: palabra, valor: definicion
+    """
+    palabras_elegidas = seleccionar_palabras_aleatoria(list(diccionario_palabras_definiciones.keys()),cantidad_letras_rosco)
+    lista_palabras_def_elegidas = extraer_claves_coincidentes(diccionario_palabras_definiciones,palabras_elegidas)
+    INICIAL = 0
+    diccionario_palabras_def_elegidas = {}
+    for i in range(len(palabras_elegidas)):
+        diccionario_palabras_def_elegidas[palabras_elegidas[i][INICIAL]] = lista_palabras_def_elegidas[i]
+    return diccionario_palabras_def_elegidas
+    #CRUZ, ARIEL CARLOS LEONARDO​
+
+def generar_dicc_segun_configuracion():
+    """
+    La funcion genera un diccionario con las palabras y sus definiciones segun la configuracion del juego
+    return: diccionario con clave: Inicial de la palabra, valor: lista con [palabra , definicion]
+    """
+    diccionario_conf = designar_configuracion()
+    longitud_minima_palabra = int(diccionario_conf["LONGITUD_PALABRA_MINIMA"])
+    cantidad_letras_rosco = int(diccionario_conf["CANTIDAD_LETRAS_ROSCO"])
+    dicc_palabras_candidates = crear_dicc_palabras_candidatas(longitud_minima_palabra)
+    dicc_palabras_elegidas = generar_dicc_palabras_def_elegidas(dicc_palabras_candidates,cantidad_letras_rosco)
+    return dicc_palabras_elegidas
+    #CRUZ, ARIEL CARLOS LEONARDO​
+
+print(doctest.testmod())
